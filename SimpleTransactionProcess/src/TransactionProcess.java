2196016736d6cca7f65654e9875cfc0eba59d475
Harun <harunrich@googlemail.com>
2017-07-01 01:58:36 +0100

    imporved logic
    removed while loops and replaced them with if/else statements

--- SimpleTransactionProcess/src/TransactionProcess.java
+++ SimpleTransactionProcess/src/TransactionProcess.java
@@ -23,61 +23,63 @@ public class TransactionProcess {
         //list variables
         double balance;
         double transferBalance;
+        String printToConsole;
          //Initialize variables
         balance = 0;
-        transferBalance = 0;
+        transferBalance = 0;        
+        printToConsole = "Enter the amount you would like to ";
         
-            //reject negative balance
-           if(balance < 0){
-                System.out.println("Account Overdrawn");
-                balance += balance;
-           } else {
-            //get balance
-                System.out.println("Your balance is: " + "£" +  balance);
-           }
             //Increase balance by user input
-         
-            System.out.print("Enter the amount you would like to deposit: £");
+            System.out.println(printToConsole + "deposit: £");
             double amount;
             amount = in.nextDouble();
-                   
+            balance += amount;
             //Compute deposit
-            
-            while ((amount <= 0) || (amount <5)){
+            if((amount <= 0) || (amount <5)){
                 System.out.println("Enter an amount greater than £5");
                 amount = in.nextDouble();
                 balance += balance;
-            } if(amount >= 5){
-                System.out.println("Balance: " + "£" + balance);              
-            }
+            } else if(amount >= 5){
+                System.out.println("Balance: £" + balance);              
+            } else{ //reject negative balance
+                    System.out.println("Account Overdrawn");
+           } 
+            //Add the decision to deposit or to transfer
             
             //Withdraw a given amount
-            System.out.println("Enter amount you would like to withdraw: ");
-            double removeAmount;
-            removeAmount = in.nextDouble();
-            while(balance < removeAmount)
-            {
-                System.out.println("Insufficient funds");
-                System.out.println("Enter amount you would like to withdraw: ");
-                removeAmount = in.nextDouble();    
-                balance -=removeAmount;
+            System.out.println("Enter the amount you would like to withdraw: ");
+            double withdrawal;
+            withdrawal = in.nextDouble();
+            if(balance < withdrawal){
+                while(balance < withdrawal)
+                {
+                    System.out.println("Insufficient funds");
+                    System.out.println("Your avaliable balance is £" + balance);
+                    System.out.println("Enter the amount you would like to withdraw: £");
+                    withdrawal = in.nextDouble();    
+                }
             }
+            balance -= withdrawal;
             System.out.println("Your new balance is: " + "£" + balance);
             
             //Send given amount to new balance
-            System.out.println("Enter amount you would like to send: ");
+            System.out.println("Enter the amount you would like to transfer: £");
             double transferAmount;
             transferAmount = in.nextDouble();
-            while(balance < transferAmount)
+            if(balance < transferAmount)
             {
                 System.out.println("Insufficient funds");
-                System.out.println("Enter amount you would like to withdraw: £");
+            } else {
+                System.out.println("Enter the amount you would like to transfer: £");
                 transferAmount = in.nextDouble();
                 transferBalance += transferAmount;
                 System.out.println("You have transfered: " + "£" + transferBalance);
+                //Print Remaining balance
+                System.out.println("Account balance: " + "£" + balance);
+                
+                //Print balance to be transfered
+                //System.out.println("Transfer balance: " + "£" + transferBalance);
             }
-            System.out.println("Account balance: " + "£" + balance);
-            System.out.println("Transfer balance: " + "£" + transferBalance);
     }
     
 }
